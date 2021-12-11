5294c3d04893019919290d752f34c0e4484cd040
brandon griffin <brandon.griffin@gmail.com>
2021-12-11 09:12:28 -0500

    remove dummt expense data

--- src/app.js
+++ src/app.js
@@ -12,19 +12,10 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    //console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({ description: 'water bill', amount: 175 }));
-store.dispatch(addExpense({ description: 'gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'rent', amount: 109500 }));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
 );
+
 ReactDOM.render(jsx, document.getElementById('app'));
