2df9771330632efa8a3c3083d581d6650cf7ddf5
brandon griffin <brandon.griffin@gmail.com>
2021-12-19 23:22:40 -0500

    setup startRemoveExpense -course version

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -9,7 +9,7 @@ export class EditExpensePage extends React.Component{
         this.props.history.push('/');
     };
     onRemove = () => {
-        this.props.removeExpense({id: this.props.expense.id });
+        this.props.startRemoveExpense({id: this.props.expense.id });
         this.props.history.push('/');
     };
     render() {
@@ -31,7 +31,7 @@ const mapStateToProps = (state, props) => ({
 
 const mapDispatchToProps = (dispatch, props) => ({
     editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-    removeExpense: (id) => dispatch(startRemoveExpense(id))
+    startRemoveExpense: (id) => dispatch(startRemoveExpense(id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
